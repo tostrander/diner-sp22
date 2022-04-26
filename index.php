dd6c0367cc5d5a36531225d4f4d3c56ad570f6a4
Tina Ostrander <tostrander@greenriver.edu>
2022-04-26 12:19:26 -0400

    Update order view

--- index.php
+++ index.php
@@ -18,5 +18,37 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+//Define a breakfast route
+$f3->route('GET /breakfast', function() {
+    //echo "Breakfast page";
+
+    $view = new Template();
+    echo $view->render('views/breakfast-menu.html');
+});
+
+//Define a lunch route
+$f3->route('GET /lunch', function() {
+    //echo "Breakfast page";
+
+    $view = new Template();
+    echo $view->render('views/lunch.html');
+});
+
+//Define a lunch route
+$f3->route('GET /breakfast/brunch', function() {
+    //echo "Breakfast page";
+
+    $view = new Template();
+    echo $view->render('views/breakfast-menu.html');
+});
+
+//Define an order route
+$f3->route('GET /order', function() {
+    //echo "Order page";
+
+    $view = new Template();
+    echo $view->render('views/orderForm1.html');
+});
+
 //Run fat-free
 $f3->run();
